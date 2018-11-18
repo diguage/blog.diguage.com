b515ef8bd3a7c5b5f4ab56f79b95e355dae9f291
diguage <leejun119@gmail.com>
2018-11-18 23:52:30 +0800

    add content

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 # 构建脚本
 
 base_dir=`pwd`
+build_dir=${base_dir}/public
+
+# rm -rf $build_dir
 
 docker run -u $UID --privileged -v `pwd`:/antora --rm -t antora/antora --cache-dir=./.cache/antora --pull site.yml
 
@@ -13,13 +16,15 @@ echo ''
 echo ''
 
 if [ -n "$1" ]; then
-    cd ${base_dir}/public
+    cd $build_dir
     rsync -avz . deployer@notes.diguage.com:/home/deployer/diguage.com/blog
+    echo ''
+    echo ''
     echo "`date '+%Y-%m-%d %H:%M:%S'` deploy"
     echo ''
     echo ''
 fi
 
-cd ${base_dir}/public
+cd $build_dir
 
 python3 -m http.server $((1024 + RANDOM % 64511)) --bind 127.0.0.1
